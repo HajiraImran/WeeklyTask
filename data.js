b31bb90daf8dd3a188593d55a01fa3c400f8c517
unknown <hajiraimran900@gmail.com>
2025-02-23 21:36:59 +0500

    task

---
+++ data.js
@@ -0,0 +1,9 @@
+function getData() {
+    return [
+        { id: 1, name: "Emerald", type: "Gemstone" },
+        { id: 2, name: "Nipried", type: "Mineral" },
+        { id: 3, name: "Copper", type: "Metal" }
+    ];
+}
+
+module.exports = getData;
